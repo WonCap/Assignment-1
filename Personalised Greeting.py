b1b2c267c46003ee2070a2ccfcf8501637e83ef5
WonCap <capsulewonder@gmail.com>
2025-03-15 22:12:44 +0530

    Update and rename Personalised Greetiong.py to Personalised Greeting.py

--- Personalised Greeting.py
+++ Personalised Greeting.py
@@ -1,3 +1,3 @@
 a=input("Enter your first name: ")
 b=input("Enter your last name: ")
-print(f"Hello, {a+" "+b}! Welcome to the Python Program")
+print(f"Hello, {a+" "+b}! Welcome to the Python Program.")
